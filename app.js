739428b48184e1fabbd3e97b27990dda1f633c96
cky <910470585@qq.com>
2022-08-12 17:49:03 +0800

    跨域

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const express = require('express')
 const app = express()
 const expressip = require('express-ip');
 
+
+
 app.use(expressip().getIpInfoMiddleware)
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+  });
+
 app.get('/',(req,res)=>{
 
     res.header('Access-Control-Allow-Origin','*')
